5d41cc649691c46b471ef3ef46c4989127dfa490
FiorenMas <102145692+FiorenMas@users.noreply.github.com>
2023-04-30 22:09:02 +0700

    Update patch.sh

--- src/tiktok/patch.sh
+++ src/tiktok/patch.sh
@@ -7,6 +7,9 @@ EXCLUDE_PATCHES=()
 for word in $(cat src/tiktok/exclude-patches.txt) ; do
     EXCLUDE_PATCHES+=("-e $word")
 done
+echo "⏬ Downloading apkeep resources..."
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/EFForg/apkeep/releases/latest/download/apkeep-x86_64-unknown-linux-gnu -o ${DIR_TMP}/apkkeep
+echo "⏬ Downloading Titok..."
 chmod +x ${DIR_TMP}/apkkeep && ${DIR_TMP}/apkkeep -a com.ss.android.ugc.trill .
+echo "⚙️ Patching Tiktok..."
 java -jar revanced-cli*.jar -m revanced-integrations*.apk -b revanced-patches*.jar ${EXCLUDE_PATCHES[@]} -a com.ss.android.ugc.trill.apk --keystore=ks.keystore -o Tiktok.apk

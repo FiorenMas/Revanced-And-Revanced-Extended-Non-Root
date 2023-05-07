d681894cf72eb4b26daef07b7a21e1e86219146f
Fioren <102145692+FiorenMas@users.noreply.github.com>
2023-05-07 16:50:30 +0700

    Update twitter.sh

--- src/build/twitter.sh
+++ src/build/twitter.sh
@@ -7,5 +7,6 @@ dl_revanced_integrations "revanced" "0.103.0"
 
 # Patch Twitter
 get_patches_key "twitter"
+version="9.87.0-release.0"
 get_apk "twitter" "twitter" "twitter-inc/twitter/twitter"
 patch "twitter" "twitter-revanced"

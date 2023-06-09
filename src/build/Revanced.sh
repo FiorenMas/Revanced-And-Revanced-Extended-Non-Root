163e0ec3dd1e2c4247007db1cf1b3a14031b46bb
Fioren <102145692+FiorenMas@users.noreply.github.com>
2023-06-09 17:28:32 +0700

    Update Revanced.sh

--- src/build/Revanced.sh
+++ src/build/Revanced.sh
@@ -30,7 +30,7 @@ get_apk "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagr
 patch "instagram-arm64-v8a" "instagram-arm64-v8a-revanced"
 # Armeabi-v7a
 get_patches_key "instagram"
-version="275.0.0.27.98"
+version="271.1.0.21.84"
 get_apk "instagram-armeabi-v7a" "instagram-instagram" "instagram/instagram-instagram/instagram-instagram" "armeabi-v7a"
 patch "instagram-armeabi-v7a" "instagram-armeabi-v7a-revanced"
 

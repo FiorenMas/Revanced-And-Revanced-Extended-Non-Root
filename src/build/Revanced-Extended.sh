0787aef62b50157fbba69cc92a8d84b398fafe5b
Fioren <102145692+FiorenMas@users.noreply.github.com>
2023-07-11 20:54:26 +0700

    Update Revanced-Extended.sh

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -16,7 +16,7 @@ dl_gh "revanced-patches revanced-cli revanced-integrations" "inotia00" "latest"
 
 # Patch YouTube Extended:
 get_patches_key "youtube-revanced-extended"
-get_ver "Hide ads" "com.google.android.youtube"
+get_ver "Hide general ads" "com.google.android.youtube"
 get_apk "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "youtube-revanced-extended"
 

ebe74ae78ff45d8b56aa75e6a0f3f96d664d9330
siritami <102145692+FiorenMas@users.noreply.github.com>
2023-08-07 00:35:34 +0700

    fix rvx android 6,7

--- src/build/Revanced-Extended-Android-6-7.sh
+++ src/build/Revanced-Extended-Android-6-7.sh
@@ -11,7 +11,7 @@ checker "kitadai31/revanced-patches-android6-7" "revanced-extended-android-6-7"
 
 # Download requirements
 dl_gh "revanced-patches-android6-7 revanced-integrations" "kitadai31" "latest"
-dl_gh "revanced-cli" "inotia00" "latest"
+dl_gh "revanced-cli" "revanced" "latest"
 dl_htmlq
 
 #################################################

f64333a9383d9d4371f27d8a170739d6bd04ed2f
Fioren <102145692+FiorenMas@users.noreply.github.com>
2023-10-10 11:44:06 +0700

    fix

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -45,8 +45,8 @@ patch "reddit" "revanced-extended" "rve"
 
 # Split architecture:
 rm -f revanced-cli* revanced-patches*.jar patches.json 
-dl_gh "revanced-cli" "j-hc" "latest"
-dl_gh "revanced-patches" "revanced" "latest"
+dl_gh "revanced-cli" "inotia00" "latest"
+dl_gh "revanced-patches" "inotia00" "latest"
 # Split architecture Youtube:
 for i in {0..3}; do
     split_arch "youtube-revanced-extended" "youtube-${archs[i]}-revanced-extended" "$(gen_rip_libs ${libs[i]})"

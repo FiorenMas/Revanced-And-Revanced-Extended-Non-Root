4fe5f357d463a7355c47e0bd5faca8031179ff03
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-04-05 09:04:53 +0700

    correct version

--- src/build/Revanced-1.sh
+++ src/build/Revanced-1.sh
@@ -20,10 +20,12 @@ patch "youtube" "revanced"
 # Patch YouTube Music:
 # Arm64-v8a
 get_patches_key "youtube-music-revanced"
+version="6.45.54"
 get_apk "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-arm64-v8a" "revanced"
 # Armeabi-v7a
 get_patches_key "youtube-music-revanced"
+version="6.45.54"
 get_apk "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
 patch "youtube-music-armeabi-v7a" "revanced"
 

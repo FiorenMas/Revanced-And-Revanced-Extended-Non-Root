c554832df1e603e916377586cb30ab627696f675
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-05-09 06:04:38 +0700

    fix last rve

--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -11,7 +11,7 @@ dl_gh "revanced-patches revanced-integrations revanced-cli" "inotia00" "latest"
 
 # Patch YouTube Extended:
 get_patches_key "youtube-revanced-extended"
-get_ver "Hide general ads" "com.google.android.youtube"
+get_ver "Video playback" "com.google.android.youtube"
 get_apk "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "revanced-extended" "inotia"
 
@@ -20,21 +20,14 @@ patch "youtube" "revanced-extended" "inotia"
 # Patch YouTube Music Extended:
 # Arm64-v8a
 get_patches_key "youtube-music-revanced-extended"
-get_ver "Hide general ads" "com.google.android.apps.youtube.music"
+get_ver "Video playback" "com.google.android.apps.youtube.music"
 get_apk "youtube-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-music-arm64-v8a" "revanced-extended" "inotia"
 # Armeabi-v7a
 get_patches_key "youtube-music-revanced-extended"
-get_ver "Hide general ads" "com.google.android.apps.youtube.music"
+get_ver "Video playback" "com.google.android.apps.youtube.music"
 get_apk "youtube-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
 patch "youtube-music-armeabi-v7a" "revanced-extended" "inotia"
-
-#################################################
-
-# Patch Reddit:
-get_patches_key "reddit-rve"
-get_apk "reddit" "reddit" "redditinc/reddit/reddit"
-patch "reddit" "revanced-extended" "inotia"
 
 #################################################
 
@@ -42,5 +35,14 @@ patch "reddit" "revanced-extended" "inotia"
 for i in {0..3}; do
     split_arch "youtube-revanced-extended" "youtube-${archs[i]}-revanced-extended" "$(gen_rip_libs ${libs[i]})"
 done
+
+#################################################
+
+# Patch Reddit:
+rm -f revanced-cli*
+dl_gh "revanced-cli" "inotia00" "v2.21.2-arsclib"
+get_patches_key "reddit-rve"
+get_apk "reddit" "reddit" "redditinc/reddit/reddit"
+patch "reddit" "revanced-extended" "inotia"
 
 #################################################

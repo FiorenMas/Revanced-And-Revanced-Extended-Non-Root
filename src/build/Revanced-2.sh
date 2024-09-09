6c1750a113c99668305e92b6bdf051dc4ed0860e
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-09-09 10:24:07 +0700

    update

--- src/build/Revanced-2.sh
+++ src/build/Revanced-2.sh
@@ -7,6 +7,14 @@ source ./src/build/utils.sh
 # Download requirements
 dl_gh "revanced-patches revanced-cli revanced-integrations" "revanced" "latest"
 
+##################################################################################################
+
+# Patch Facebook:
+# Arm64-v8a
+get_patches_key "facebook"
+get_apk "com.facebook.katana" "facebook-arm64-v8a" "facebook" "facebook-2/facebook/facebook" "arm64-v8a" "nodpi" "Android 11+"
+patch "facebook-arm64-v8a" "revanced"
+
 #################################################
 
 # Patch Twitch:
--- src/build/Revanced-Beta-2.sh
+++ src/build/Revanced-Beta-2.sh
@@ -8,6 +8,14 @@ source ./src/build/utils.sh
 dl_gh "revanced-patches revanced-integrations" "revanced" "prerelease"
 dl_gh "revanced-cli" "revanced" "latest"
 
+##################################################################################################
+
+# Patch Facebook:
+# Arm64-v8a
+get_patches_key "facebook"
+get_apk "com.facebook.katana" "facebook-arm64-v8a-beta" "facebook" "facebook-2/facebook/facebook" "arm64-v8a" "nodpi" "Android 11+"
+patch "facebook-arm64-v8a-beta" "revanced"
+
 #################################################
 
 # Patch Twitch:

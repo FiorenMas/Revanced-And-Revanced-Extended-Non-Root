2f29b54d784fca53dee864a4a46190fc551a82ad
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-09-11 15:58:28 +0700

    fix latest version reddit

--- src/build/Revanced-Extended-Reddit.sh
+++ src/build/Revanced-Extended-Reddit.sh
@@ -12,7 +12,7 @@ dl_gh "revanced-patches-arsclib revanced-integrations revanced-cli-arsclib" "ino
 # Patch Reddit:
 get_patches_key "reddit-rve"
 #version="2024.25.3"
-get_apk "com.reddit.frontpage" "reddit" "reddit" "redditinc/reddit/reddit"
+get_apk "com.reddit.frontpage" "reddit" "reddit" "redditinc/reddit/reddit" "Bundle"
 patch "reddit" "revanced-extended"
 mv ./release/reddit-revanced-extended.apk/base.apk ./reddit-revanced-extended.apk
 rm -f -d ./release/reddit-revanced-extended.apk

b5c2e46062f574bb9a50b80e00d1eb11ee82ab26
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-10-21 03:39:16 +0700

    correct version

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -41,6 +41,7 @@ revanced_dl(){
 	# Patch Facebook:
 	# Arm64-v8a
 	get_patches_key "facebook"
+ 	version="485.0.0.70.77"
 	get_apk "com.facebook.katana" "facebook-arm64-v8a-beta" "facebook" "facebook-2/facebook/facebook" "arm64-v8a" "nodpi" "Android 11+"
 	patch "facebook-arm64-v8a-beta" "revanced"
 }

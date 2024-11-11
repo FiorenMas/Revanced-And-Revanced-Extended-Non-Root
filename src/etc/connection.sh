3dcbd89dbe9d97c206398de3936428f613c1cce4
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-11-11 09:02:59 +0700

    update

--- src/etc/connection.sh
+++ src/etc/connection.sh
@@ -2,7 +2,7 @@
 
 # Check github connection stable or not:
 check_connection() {
-	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/latest" | jq -r '.assets[] | select(.name == test("^.*\\\.rvp$")) | .browser_download_url') || rm -f "patches.json"
+	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/latest" | jq -r '.assets[] | select(.name | test("^.*\\.rvp$")) | .browser_download_url') || rm -f "*.rvp"
 	if [ -f *.rvp ]; then
 		echo "internet_error=0" >> $GITHUB_OUTPUT
 		echo -e "\e[32mGithub connection OK\e[0m"

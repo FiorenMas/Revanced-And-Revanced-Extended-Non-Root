b870605f40fd4b49243a79b28426d900ccd722a4
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-11-11 16:17:43 +0700

    fix

--- src/etc/connection.sh
+++ src/etc/connection.sh
@@ -2,10 +2,11 @@
 
 # Check github connection stable or not:
 check_connection() {
-	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/latest" | jq -r '.assets[] | select(.name | test("^.*\\.rvp$")) | .browser_download_url') || rm -f "*.rvp"
-	if [ -f *.rvp ]; then
+	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/tags/v4.17.0" | jq -r '.assets[] | select(.name == "patches.json") | .browser_download_url') || rm -f "patches.json"
+	if [ -f patches.json ]; then
 		echo "internet_error=0" >> $GITHUB_OUTPUT
 		echo -e "\e[32mGithub connection OK\e[0m"
+		rm -f "patches.json"
 	else
 		echo "internet_error=1" >> $GITHUB_OUTPUT
 		echo -e "\e[31mGithub connection not stable!\e[0m"

25fc426751d7fb7c58dfe212d44e636600ff7132
Fioren <102145692+FiorenMas@users.noreply.github.com>
2024-12-21 23:57:20 +0700

    fix lastest rve anddea

--- src/build/Anddea-Revanced-Extended-Beta.sh
+++ src/build/Anddea-Revanced-Extended-Beta.sh
@@ -9,27 +9,37 @@ revanced_dl(){
 
 1() {
 	revanced_dl
+	#Disabled because lastest RVE Anddea patch youtube not have splits apk on APKMirror
+	# Patch YouTube:
+	#get_patches_key "youtube-rve-anddea"
+	#get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
+	#split_editor "youtube-beta" "youtube-beta"
+	#patch "youtube-beta" "anddea" "inotia"
+	# Patch Youtube Arm64-v8a
+	#get_patches_key "youtube-rve-anddea"
+	#split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
+	#patch "youtube-beta-arm64-v8a" "anddea" "inotia"
+	# Patch Youtube Armeabi-v7a
+	#get_patches_key "youtube-rve-anddea"
+	#split_editor "youtube-beta" "youtube-beta-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
+	#patch "youtube-beta-armeabi-v7a" "anddea" "inotia"
+	# Patch Youtube x86
+	#get_patches_key "youtube-rve-anddea"
+	#split_editor "youtube-beta" "youtube-beta-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
+	#patch "youtube-beta-x86" "anddea" "inotia"
+	# Patch Youtube x86_64
+	#get_patches_key "youtube-rve-anddea"
+	#split_editor "youtube-beta" "youtube-beta-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
+	#patch "youtube-beta-x86_64" "anddea" "inotia"
 	# Patch YouTube:
 	get_patches_key "youtube-rve-anddea"
-	get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-	split_editor "youtube-beta" "youtube-beta"
+	get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
 	patch "youtube-beta" "anddea" "inotia"
-	# Patch Youtube Arm64-v8a
+	# Split architecture Youtube:
 	get_patches_key "youtube-rve-anddea"
-	split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
-	patch "youtube-beta-arm64-v8a" "anddea" "inotia"
-	# Patch Youtube Armeabi-v7a
-	get_patches_key "youtube-rve-anddea"
-	split_editor "youtube-beta" "youtube-beta-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
-	patch "youtube-beta-armeabi-v7a" "anddea" "inotia"
-	# Patch Youtube x86
-	get_patches_key "youtube-rve-anddea"
-	split_editor "youtube-beta" "youtube-beta-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
-	patch "youtube-beta-x86" "anddea" "inotia"
-	# Patch Youtube x86_64
-	get_patches_key "youtube-rve-anddea"
-	split_editor "youtube-beta" "youtube-beta-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
-	patch "youtube-beta-x86_64" "anddea" "inotia"
+	for i in {0..3}; do
+		split_arch "youtube-beta" "anddea" "$(gen_rip_libs ${libs[i]})"
+	done
 }
 2() {
 	revanced_dl
@@ -44,16 +54,17 @@ revanced_dl(){
 	patch "youtube-music-beta-armeabi-v7a" "anddea" "inotia"
 }
 3() {
-	revanced_dl
-	get_apk "com.google.android.youtube" "youtube-lite-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
+	#revanced_dl
+	#Disabled because lastest RVE Anddea patch youtube not have splits apk on APKMirror
+	#get_apk "com.google.android.youtube" "youtube-lite-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
 	# Patch YouTube Lite Arm64-v8a:
-	get_patches_key "youtube-rve-anddea"
-	split_editor "youtube-lite-beta" "youtube-lite-beta-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xhdpi split_config.xxxhdpi"
-	patch "youtube-lite-beta-arm64-v8a" "anddea" "inotia"
+	#get_patches_key "youtube-rve-anddea"
+	#split_editor "youtube-lite-beta" "youtube-lite-beta-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xhdpi split_config.xxxhdpi"
+	#patch "youtube-lite-beta-arm64-v8a" "anddea" "inotia"
 	# Patch YouTube Lite Armeabi-v7a:
-	get_patches_key "youtube-rve-anddea"
-	split_editor "youtube-lite-beta" "youtube-lite-beta-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
-	patch "youtube-lite-beta-armeabi-v7a" "anddea" "inotia"
+	#get_patches_key "youtube-rve-anddea"
+	#split_editor "youtube-lite-beta" "youtube-lite-beta-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
+	#patch "youtube-lite-beta-armeabi-v7a" "anddea" "inotia"
 }
 case "$1" in
     1)

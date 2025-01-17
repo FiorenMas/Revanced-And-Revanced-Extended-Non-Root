32fce390c5a8c078ed980775a7876d9b08d471cc
siritami <102145692+FiorenMas@users.noreply.github.com>
2025-01-17 14:39:29 +0700

    fix

--- src/build/Revanced-Extended-Beta.sh
+++ src/build/Revanced-Extended-Beta.sh
@@ -44,9 +44,9 @@ get_patches_key "youtube-music-revanced-extended"
 get_apk "com.google.android.apps.youtube.music" "youtube-beta-music-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 patch "youtube-beta-music-arm64-v8a" "revanced-extended" "inotia"
 # Armeabi-v7a
-get_patches_key "youtube-music-revanced-extended"
-get_apk "com.google.android.apps.youtube.music" "youtube-beta-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
-patch "youtube-beta-music-armeabi-v7a" "revanced-extended" "inotia"
+#get_patches_key "youtube-music-revanced-extended"
+#get_apk "com.google.android.apps.youtube.music" "youtube-beta-music-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
+#patch "youtube-beta-music-armeabi-v7a" "revanced-extended" "inotia"
 
 # Patch Reddit:
 get_patches_key "reddit-rve"
@@ -54,9 +54,9 @@ get_apk "com.reddit.frontpage" "reddit-beta" "reddit" "redditinc/reddit/reddit" 
 split_editor "reddit-beta" "reddit"
 patch "reddit-beta" "revanced-extended" "inotia"
 # Patch Arm64-v8a:
-#split_editor "reddit-beta" "reddit-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-#get_patches_key "reddit-rve"
-#patch "reddit-arm64-v8a-beta" "revanced-extended" "inotia"
+split_editor "reddit-beta" "reddit-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+get_patches_key "reddit-rve"
+patch "reddit-arm64-v8a-beta" "revanced-extended" "inotia"
 
 #Disabled because lastest RVE patch youtube not have splits apk on APKMirror
 #get_apk "com.google.android.youtube" "youtube-lite-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"

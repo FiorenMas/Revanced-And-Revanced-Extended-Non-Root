0ffe04eceb019b44f47fa577bae3aed5bc49e824
Fioren <102145692+FiorenMas@users.noreply.github.com>
2025-05-24 17:57:11 +0700

    fix

--- src/build/Spotjfy.sh
+++ src/build/Spotjfy.sh
@@ -8,5 +8,5 @@ dl_gh "revanced-patches revanced-cli" "revanced" "latest"
 # Patch Spotjfy Arm64-v8a
 get_patches_key "Spotjfy-revanced"
 j="i"
-get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
+get_apkpure "com.spot"$j"fy.music" "spotjfy-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music" "Bundle"
 patch "spotjfy-arm64-v8a" "revanced"

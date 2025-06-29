973600e2fe70783ee4b78c370c318fda9a983b0a
Fioren <102145692+FiorenMas@users.noreply.github.com>
2025-06-29 14:32:14 +0700

    correct version

--- src/build/Revanced-Experiment.sh
+++ src/build/Revanced-Experiment.sh
@@ -22,5 +22,6 @@ patch "telegram-web-version" "revanced-experiments"
 
 # Patch Instagram:
 get_patches_key "instagram-revanced-experiments"
+version="362.0.0.33.241"
 get_apk "com.instagram.android" "instagram-arm64-v8a" "instagram-instagram" "instagram/instagram-instagram/instagram" "arm64-v8a" "nodpi"
 patch "instagram-arm64-v8a" "revanced-experiments"

6009d3995cb371c4a9e412d4e67ddebfb5f821f8
yue374 <102145692+FiorenMas@users.noreply.github.com>
2025-09-17 18:35:17 +0700

    add Proton mail + bump version apps
    Closed https://github.com/FiorenMas/Revanced-And-Revanced-Extended-Non-Root/issues/78

--- src/build/Revanced-Beta.sh
+++ src/build/Revanced-Beta.sh
@@ -190,11 +190,14 @@ revanced_dl(){
 12() {
 	revanced_dl
 	# Patch Spotjfy Arm64-v8a
-	get_patches_key "Spotjfy-revanced"
 	j="i"
- 	version="9.0.64.107" #https://github.com/ReVanced/revanced-patches/issues/5537#issuecomment-3134402120
+	get_patches_key "Spotjfy-revanced"
 	get_apkpure "com.spot"$j"fy.music" "spotjfy-beta-arm64-v8a" "spot"$j"fy-music-and-podcasts-for-android/com.spot"$j"fy.music"
 	patch "spotjfy-beta-arm64-v8a" "revanced"
+	# Patch Proton mail
+	get_patches_key "protonmail-revanced"
+	get_apk "ch.protonmail.android" "protonmail-beta" "protonmail-encrypted-email" "proton-technologies-ag/protonmail-encrypted-email/proton-mail-encrypted-email"
+	patch "protonmail-beta" "revanced"
 }
 case "$1" in
     1)

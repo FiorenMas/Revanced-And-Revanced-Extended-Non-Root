c46f7e565577853effbc3ee5f570f8c36a831dd4
yue374 <102145692+FiorenMas@users.noreply.github.com>
2025-09-24 19:55:29 +0700

    Fix new Piko dev patch
    Piko dev patch update to new api so need change code to patch it.

--- src/build/Piko.sh
+++ src/build/Piko.sh
@@ -2,23 +2,15 @@
 # Twitter Piko
 source src/build/utils.sh
 
+dl_gh "revanced-cli" "revanced" "v4.6.0"
+dl_gh "piko revanced-integrations" "crimera" "latest"
+
 # Patch Twitter Piko:
-patch_piko () {
-	dl_gh "revanced-cli" "revanced" "v4.6.0"
-	get_patches_key "twitter-piko"
-	local v apk_name
-	if [[ "$1" == "latest" ]]; then
-		v="latest" apk_name="stable"
-	else
-		v="prerelease" apk_name="beta"
-	fi
-	dl_gh "piko revanced-integrations" "crimera" "$v"
-	get_apk "com.twitter.android" "twitter-$apk_name" "twitter" "x-corp/twitter/x-previously-twitter" "Bundle_extract"
-	split_editor "twitter-$apk_name" "twitter-$apk_name"
-	patch "twitter-$apk_name" "piko"
-	# Patch Twitter Piko Arm64-v8a:
-	get_patches_key "twitter-piko"
-	split_editor "twitter-$apk_name" "twitter-arm64-v8a-$apk_name" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-	patch "twitter-arm64-v8a-$apk_name" "piko"
-}
-patch_piko $1
+get_patches_key "twitter-piko"
+get_apk "com.twitter.android" "twitter-stable" "twitter" "x-corp/twitter/x-formerly-twitter" "Bundle_extract"
+split_editor "twitter-stable" "twitter-stable"
+patch "twitter-stable" "piko"
+# Patch Twitter Piko Arm64-v8a:
+get_patches_key "twitter-piko"
+split_editor "twitter-stable" "twitter-arm64-v8a-stable" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+patch "twitter-arm64-v8a-stable" "piko"

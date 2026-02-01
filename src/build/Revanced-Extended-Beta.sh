8f9481408c5e621ac9567e69c68886eae698cc3e
siritami <102145692+FiorenMas@users.noreply.github.com>
2026-02-01 10:10:18 +0700

    fix rve

--- src/build/Revanced-Extended-Beta.sh
+++ src/build/Revanced-Extended-Beta.sh
@@ -7,34 +7,24 @@ dl_gh "revanced-patches revanced-cli" "inotia00" "prerelease"
 
 # Patch YouTube:
 get_patches_key "youtube-revanced-extended"
-get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-split_editor "youtube-beta" "youtube-beta"
+get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
 patch "youtube-beta" "revanced-extended" "inotia"
+# Remove unused architectures
+for i in {0..3}; do
+	apk_editor "youtube-beta" "${archs[i]}" ${libs[i]}
+done
 # Patch Youtube Arm64-v8a
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube-beta" "youtube-beta-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "youtube-beta-arm64-v8a" "revanced-extended" "inotia"
 # Patch Youtube Armeabi-v7a
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube-beta" "youtube-beta-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
 patch "youtube-beta-armeabi-v7a" "revanced-extended" "inotia"
 # Patch Youtube x86
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube-beta" "youtube-beta-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
 patch "youtube-beta-x86" "revanced-extended" "inotia"
 # Patch Youtube x86_64
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube-beta" "youtube-beta-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
 patch "youtube-beta-x86_64" "revanced-extended" "inotia"
-# Patch YouTube:
-#get_patches_key "youtube-revanced-extended"
-#get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
-#patch "youtube-beta" "revanced-extended" "inotia"
-# Split architecture Youtube:
-#get_patches_key "youtube-revanced-extended"
-#for i in {0..3}; do
-#  split_arch "youtube-beta" "revanced-extended" "$(gen_rip_libs ${libs[i]})"
-#done
 
 # Patch YouTube Music Extended:
 # Arm64-v8a
@@ -64,12 +54,3 @@ patch "reddit-beta" "revanced-extended" "inotia"
 split_editor "reddit-beta" "reddit-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
 get_patches_key "reddit-rve"
 patch "reddit-arm64-v8a-beta" "revanced-extended" "inotia"
-
-# Patch YouTube Lite Arm64-v8a:
-get_patches_key "youtube-revanced-extended"
-split_editor "youtube-beta" "youtube-lite-beta-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xhdpi split_config.xxxhdpi"
-patch "youtube-lite-beta-arm64-v8a" "revanced-extended" "inotia"
-# Patch YouTube Lite Armeabi-v7a:
-get_patches_key "youtube-revanced-extended"
-split_editor "youtube-beta" "youtube-lite-beta-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
-patch "youtube-lite-beta-armeabi-v7a" "revanced-extended" "inotia"
--- src/build/Revanced-Extended.sh
+++ src/build/Revanced-Extended.sh
@@ -7,34 +7,24 @@ dl_gh "revanced-patches revanced-cli" "inotia00" "latest"
 
 # Patch YouTube:
 get_patches_key "youtube-revanced-extended"
-get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
-split_editor "youtube" "youtube"
+get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube"
 patch "youtube" "revanced-extended" "inotia"
+# Remove unused architectures
+for i in {0..3}; do
+	apk_editor "youtube" "${archs[i]}" ${libs[i]}
+done
 # Patch Youtube Arm64-v8a
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube" "youtube-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64"
 patch "youtube-arm64-v8a" "revanced-extended" "inotia"
 # Patch Youtube Armeabi-v7a
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube" "youtube-armeabi-v7a" "exclude" "split_config.arm64_v8a split_config.x86 split_config.x86_64"
 patch "youtube-armeabi-v7a" "revanced-extended" "inotia"
 # Patch Youtube x86
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube" "youtube-x86" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86_64"
 patch "youtube-x86" "revanced-extended" "inotia"
 # Patch Youtube x86_64
 get_patches_key "youtube-revanced-extended"
-split_editor "youtube" "youtube-x86_64" "exclude" "split_config.arm64_v8a split_config.armeabi_v7a split_config.x86"
 patch "youtube-x86_64" "revanced-extended" "inotia"
-# Patch YouTube:
-#get_patches_key "youtube-revanced-extended"
-#get_apk "com.google.android.youtube" "youtube" "youtube" "google-inc/youtube/youtube"
-#patch "youtube" "revanced-extended" "inotia"
-# Split architecture Youtube:
-#get_patches_key "youtube-revanced-extended"
-#for i in {0..3}; do
-#	split_arch "youtube" "revanced-extended" "$(gen_rip_libs ${libs[i]})"
-#done
 
 # Patch YouTube Music Extended:
 # Arm64-v8a
@@ -63,12 +53,3 @@ patch "reddit" "revanced-extended" "inotia"
 split_editor "reddit" "reddit-arm64-v8a" "exclude" "split_config.armeabi_v7a split_config.x86_64 split_config.mdpi split_config.ldpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
 get_patches_key "reddit-rve"
 patch "reddit-arm64-v8a" "revanced-extended" "inotia"
-
-# Patch YouTube Lite Arm64-v8a:
-get_patches_key "youtube-revanced-extended"
-split_editor "youtube" "youtube-lite-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xhdpi split_config.xxxhdpi"
-patch "youtube-lite-arm64-v8a" "revanced-extended" "inotia"
-# Patch YouTube Lite Armeabi-v7a:
-get_patches_key "youtube-revanced-extended"
-split_editor "youtube" "youtube-lite-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
-patch "youtube-lite-armeabi-v7a" "revanced-extended" "inotia"

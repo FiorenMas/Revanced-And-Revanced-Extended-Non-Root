e1805c595aeec83e8ee3afa86c2b631b799f08a9
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-02-13 17:09:04 +0700

    fix

--- src/build/Piko-Beta.sh
+++ src/build/Piko-Beta.sh
@@ -12,5 +12,5 @@ split_editor "twitter-beta" "twitter-beta"
 patch "twitter-beta" "piko"
 # Patch Twitter Piko Arm64-v8a:
 get_patches_key "twitter-piko"
-split_editor "twitter-beta" "twitter-arm64-v8a-beta" "exclude" "plit_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
+split_editor "twitter-beta" "twitter-arm64-v8a-beta" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
 patch "twitter-arm64-v8a-beta" "piko"

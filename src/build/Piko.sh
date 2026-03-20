7c24c7ab06676d105d6dc4ba790766ca117cff63
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-03-21 03:44:12 +0700

    fix

--- src/build/Piko.sh
+++ src/build/Piko.sh
@@ -2,15 +2,15 @@
 # Twitter Piko
 source src/build/utils.sh
 
-dl_gh "revanced-cli" "revanced" "latest"
+dl_gh "morphe-cli" "MorpheApp" "latest"
 dl_gh "piko" "crimera" "latest"
 
 # Patch Twitter Piko:
 get_patches_key "twitter-piko"
 get_apk "com.twitter.android" "twitter-stable" "twitter" "x-corp/twitter/x" "Bundle_extract"
 split_editor "twitter-stable" "twitter-stable"
-patch "twitter-stable" "piko"
+patch "twitter-stable" "piko" "morphe"
 # Patch Twitter Piko Arm64-v8a:
 get_patches_key "twitter-piko"
 split_editor "twitter-stable" "twitter-arm64-v8a-stable" "exclude" "split_config.armeabi_v7a split_config.x86 split_config.x86_64 split_config.mdpi split_config.hdpi split_config.xhdpi split_config.xxhdpi split_config.tvdpi"
-patch "twitter-arm64-v8a-stable" "piko"
+patch "twitter-arm64-v8a-stable" "piko" "morphe"

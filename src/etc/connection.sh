8faa77d7c5853a8703c42a80d211ce5d10b04690
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-03-25 23:34:17 +0700

    fix

--- src/etc/connection.sh
+++ src/etc/connection.sh
@@ -2,11 +2,11 @@
 
 # Check github connection stable or not:
 check_connection() {
-	wget -q $(curl -s "https://api.github.com/repos/revanced/revanced-patches/releases/tags/v4.17.0" | jq -r '.assets[] | select(.name == "patches.json") | .browser_download_url') || rm -f "patches.json"
-	if [ -f patches.json ]; then
+	wget -q $(curl -s "https://api.github.com/repos/MorpheApp/morphe-patches/releases/tags/v1.21.1" | jq -r '.assets[] | select(.name == "patches-1.21.1.mpp") | .browser_download_url') || rm -f "patches-1.21.1.mpp"
+	if [ -f patches-1.21.1.mpp ]; then
 		echo "internet_error=0" >> $GITHUB_OUTPUT
 		echo -e "\e[32mGithub connection OK\e[0m"
-		rm -f "patches.json"
+		rm -f "patches-1.21.1.mpp"
 	else
 		echo "internet_error=1" >> $GITHUB_OUTPUT
 		echo -e "\e[31mGithub connection not stable!\e[0m"

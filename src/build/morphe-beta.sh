85e0b894ad5fcab260db3d127e43f6d8738731d5
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-04-01 17:44:20 +0700

    bump version

--- src/build/morphe-beta.sh
+++ src/build/morphe-beta.sh
@@ -11,7 +11,7 @@ morphe_dl(){
 
 	# Patch YouTube:
 	get_patches_key "youtube-morphe"
-	prefer_version="21.11.480"
+	prefer_version="21.13.163"
 	get_apk "com.google.android.youtube" "youtube-beta" "youtube" "google-inc/youtube/youtube"
 	patch "youtube-beta" "morphe" "morphe"
 	# Remove unused architectures
@@ -23,7 +23,7 @@ morphe_dl(){
 	morphe_dl
 	# Patch YouTube Lite Arm64-v8a:
 	get_patches_key "youtube-morphe"
-	prefer_version="21.11.480"
+	prefer_version="21.13.163"
 	get_apk "com.google.android.youtube" "youtube-beta-lite" "youtube" "google-inc/youtube/youtube" "Bundle_extract"
 	split_editor "youtube-beta-lite" "youtube-beta-lite-arm64-v8a" "include" "split_config.arm64_v8a split_config.en split_config.xxxhdpi"
 	patch "youtube-beta-lite-arm64-v8a" "morphe" "morphe"
@@ -33,7 +33,7 @@ morphe_dl(){
 	patch "youtube-beta-lite-armeabi-v7a" "morphe" "morphe"
 	# Patch Reddit:
 	get_patches_key "reddit-morphe"
-	prefer_version="2026.11.0"
+	prefer_version="2026.13.0"
 	get_apk "com.reddit.frontpage" "reddit-beta" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
 	split_editor "reddit-beta" "reddit"
 	patch "reddit-beta" "morphe" "morphe"
@@ -47,22 +47,22 @@ morphe_dl(){
 	# Patch YouTube Music:
 	# Arm64-v8a
 	get_patches_key "youtube-music-morphe"
-	prefer_version="9.09.52"
+	prefer_version="9.12.51"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
 	patch "youtube-music-beta-arm64-v8a" "morphe" "morphe"
 	# Armeabi-v7a
 	get_patches_key "youtube-music-morphe"
-	prefer_version="9.09.52"
+	prefer_version="9.12.51"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
 	patch "youtube-music-beta-armeabi-v7a" "morphe" "morphe"
 	# x86_64
 	get_patches_key "youtube-music-morphe"
-	prefer_version="9.09.52"
+	prefer_version="9.12.51"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
 	patch "youtube-music-beta-x86_64" "morphe" "morphe"
 	# x86
 	get_patches_key "youtube-music-morphe"
-	prefer_version="9.09.52"
+	prefer_version="9.12.51"
 	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-x86" "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
 	patch "youtube-music-beta-x86" "morphe" "morphe"
 }

ac68e5bae8d87039bfd2afc0cf9956624b588daa
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-04-19 13:35:43 +0700

    bump version

--- src/build/morphe-beta.sh
+++ src/build/morphe-beta.sh
@@ -51,7 +51,7 @@ morphe_dl(){
 	patch "youtube-beta-lite-armeabi-v7a" "morphe" "morphe"
 	# Patch Reddit:
 	get_patches_key "reddit-morphe"
-	prefer_version="2026.10.0"
+	prefer_version="2026.16.0"
 	get_apk "com.reddit.frontpage" "reddit-beta" "reddit" "redditinc/reddit/reddit" "Bundle_extract"
 	split_editor "reddit-beta" "reddit"
 	patch "reddit-beta" "morphe" "morphe"

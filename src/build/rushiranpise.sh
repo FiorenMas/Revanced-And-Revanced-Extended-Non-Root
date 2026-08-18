a1e7152c466bc9c2c26d7f47572ebdfbc2806834
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-08-18 11:42:58 +0700

    correct patch

--- src/build/rushiranpise.sh
+++ src/build/rushiranpise.sh
@@ -10,7 +10,7 @@ rushiranpise_dl(){
 	rushiranpise_dl
 	# Patch Waze:
 	get_patches_key "waze-rushiranpise"
-	get_apk "com.waze" "waze" "bundle" "arm64-v8a" "480-640dpi" "Android 10+"
+	get_apk "com.waze" "waze" "bundle"
 	patch "waze" "rushiranpise"
 	# Patch Adguard
 	get_patches_key "adguard-rushiranpise"

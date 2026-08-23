4d5d7007225f9bccfd8ad0cbf92f1179fa0c98f8
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-08-23 10:34:26 +0700

    correct version

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -6,7 +6,7 @@ NPatch_dl(){
 	dl_gh "NPatch" "7723mod" "v1.0.5"
 }
 patch_dl(){
-	dl_gh "NexAlloy" "gnadgnaoh" "v2.2"
+	dl_gh "NexAlloy" "gnadgnaoh" "latest"
 }
 1() {
 	# Patch Revenge:
@@ -21,27 +21,27 @@ patch_dl(){
 	# Patch Facebook:
 	version="575.0.0.45.73"
 	get_apk "com.facebook.katana" "facebook-arm64-v8a" "bundle" "arm64-v8a" "160-640dpi" "Android 11+"
-	npatch "facebook-arm64-v8a" "NexAlloy*release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "facebook-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
 	# Patch Messenger:
 	get_apk "com.facebook.orca" "messenger-arm64-v8a" "apk" "arm64-v8a" "nodpi" "Android 9.0+"
-	npatch "messenger-arm64-v8a" "NexAlloy*release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "messenger-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
 }
 3() {
 	NPatch_dl
 	patch_dl
 	# Patch Instagram:
 	get_apk "com.instagram.android" "instagram-arm64-v8a" "bundle" "arm64-v8a" "120-640dpi"  "Android 9.0+"
-	npatch "instagram-arm64-v8a" "NexAlloy*release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "instagram-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
 	# Patch Thread:
 	get_apk "com.instagram.barcelona" "threads-arm64-v8a" "bundle" "arm64-v8a" "320-480dpi" "Android 9.0+"
-	npatch "threads-arm64-v8a" "NexAlloy*release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "threads-arm64-v8a" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
 }
 4() {
 	NPatch_dl
 	patch_dl
 	# Patch Zalo:
 	get_apk "com.zing.zalo" "zalo" "bundle" "arm64-v8a + armeabi-v7a"
-	npatch "zalo" "NexAlloy-zalo-release*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
+	npatch "zalo" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex --sigbypasslv 3"
 }
 case "$1" in
     1)

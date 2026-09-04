bfeb230bfceb631a3999d620d057adad0dc03930
Fioren <102145692+FiorenMas@users.noreply.github.com>
2026-09-04 10:48:36 +0700

    fix

--- src/build/Xposed.sh
+++ src/build/Xposed.sh
@@ -41,7 +41,7 @@ patch_dl(){
 	patch_dl
 	# Patch Zalo:
 	get_apk "com.zing.zalo" "zalo" "bundle" "arm64-v8a + armeabi-v7a"
-	lspatch "zalo" "NexAlloy-nonroot*.apk" "gnadgnaoh" "--injectdex"
+	lspatch "zalo" "NexAlloy-nonroot*.apk" "gnadgnaoh"
 }
 case "$1" in
     1)
